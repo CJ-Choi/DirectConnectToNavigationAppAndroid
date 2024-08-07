23be51c0d8ccda6243399fd5065e1e5abd2dd3ac
cjchoi <cjchoi@triniti.kr>
2024-08-07 11:29:00 +0900

    removed redundant codes

--- app/src/main/java/com/example/call_test_normal/ApiService.kt
+++ app/src/main/java/com/example/call_test_normal/ApiService.kt
@@ -5,15 +5,10 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("location")
-    fun sendLocation(@Body request: LocationRequest): Call<ApiResponse>
 
     @POST("sendToken")
     suspend fun sendToken(@Body request: TokenRequest): TokenResponse
 }
-
-data class LocationRequest(val lat: Float, val lng: Float, val time: String)
-data class ApiResponse(val success: Boolean, val message: String)
 data class TokenRequest(val token: String, val driverNo: Int)
 data class TokenResponse(val success: Boolean, val message: String)
 
--- app/src/main/java/com/example/call_test_normal/FirebaseMessagingService.kt
+++ app/src/main/java/com/example/call_test_normal/FirebaseMessagingService.kt
@@ -19,41 +19,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         remoteMessage.notification?.let {
             sendNotification(it.title, it.body)
         }
-
-        Log.d("onMessageReceived", "Message Received : 2")
-
-        //TODO 이거 하고 있었음 - 메세지 ID, 메세지 내용, 메세지 받은 시간 객체 할당해 내부 DB에 저장하기
-        // TODO 현재는 ID, 메세지, 시간 값이지만 추후 콜 정보로 수정 예정
-        // 메시지 보낸 시간 참조
-        val sentTime = remoteMessage.sentTime
-
-
-        Log.d("onMessageReceived", "Sent Time : $sentTime")
-
-        // 타임스탬프를 읽기 쉬운 형식으로 변환 (예: Date 객체 사용)
-        val sentDate: Date = Date(sentTime)
-        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        val formattedDate: String = sdf.format(sentDate)
-        Log.d("onMessageReceived", "formattedDate : $formattedDate")
-
-        Log.d("FCM", sdf.toString())
-        Log.d("FCM", remoteMessage.sentTime.toString())
-
-        // FCM 메시지 저장
-        val messageId = remoteMessage.messageId ?: return
-        val messageBody = remoteMessage.notification?.body ?: return
-        Log.d("onMessageReceived", "messageBody : $messageBody")
-
-        val dbHelper = FirebaseMessageStorageHelper(applicationContext)
-        dbHelper.setFcmData(messageId, messageBody, formattedDate)
-        Log.d("onMessageReceived", "setFcmData : $messageId $messageBody $formattedDate ")
-
-        val intent = Intent("FCM_MESSAGE_EVENT").apply {
-            putExtra("messageId", messageId)
-            putExtra("messageBody", messageBody)
-            putExtra("formattedDate", formattedDate)
-        }
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
 
     private fun sendNotification(title: String?, message: String?) {
@@ -67,21 +32,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d("FCM", "Refreshed token: $token")
-
-        // 토큰을 새로운 테이블에 저장
-        val dbHelper = FirebaseMessageStorageHelper(applicationContext)
-        dbHelper.setTokenData(token)
-
-        // 브로드캐스트 송신
-        val broadcastIntent = Intent().also {
-            it.action = "com.example.broadcast.TOKEN_BROADCAST"
-            it.putExtra("data", token)
-            // example로 driverNo를 12345로 설정합니다. 필요에 따라 동적 할당할 수 있습니다.
-            it.putExtra("driverNo", 12345)
-        }
-        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent)
-
-        // TODO Firestore 혹은 Retrofit API 자동 토큰 전송 및 등록
     }
 
 }
